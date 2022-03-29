a8d0a2a8f9a126745ef0543bbc0693e9c403474a
JonssonJohannae <johanna_jonson@hotmail.com>
2022-03-29 14:38:35 +0200

    Created styling to pages

--- kollektiva/pages/index.js
+++ kollektiva/pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../components/Footer";
 import Navbar from "../components/navbar";
-// import Navbar from "../components/navbar";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Startsida</h1>
     </div>
   );

14914bb5c0a0ebfd27999759cf37ceb6bdcf15ac
JonssonJohannae <johanna_jonson@hotmail.com>
2022-03-29 13:10:11 +0200

    Created about page

---
+++ kollektiva/pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        voluptatum, voluptatibus sit provident repudiandae magnam sapiente ipsum
+        quae quia cupiditate ratione dolores velit rerum laboriosam? A nihil
+        quia animi doloribus.
+      </p>
+    </div>
+  );
+};
+
+export default About;

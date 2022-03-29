7303d7a5b63f311342d7c59971de0b90252917da
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-03-29 23:07:42 +0200

    added svgs etc for front-page

--- kollektiva/pages/index.js
+++ kollektiva/pages/index.js
@@ -20,12 +20,15 @@ export default function Home() {
           <div className={styles.searchButton}>Sök bostad</div>
         </div>
       </div>
+      <div className={styles.advancedSearch}>Avancerad sökning</div>
       <div className={styles.selectContainer}>
         <button className={styles.findContainer}>
           <h1 className={styles.select}>Hitta bostad</h1>
+          <Image src="/Group.svg" width={51} height={47} />
         </button>
         <button className={styles.rentContainer}>
           <h1 className={styles.select}>Bli hyresvärd</h1>
+          <Image src="/Key.svg" width={47} height={47} />
         </button>
       </div>
     </div>

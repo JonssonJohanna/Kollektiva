ed3a86255237be85df251bc9761b3811e78960fd
JonssonJohannae <johanna_jonson@hotmail.com>
2022-03-30 13:34:59 +0200

    Edit on footer and some styling

--- kollektiva/components/Footer.js
+++ kollektiva/components/Footer.js
@@ -1,7 +1,25 @@
 const Footer = () => {
   return (
     <footer>
-      <h3>Footer</h3>
+      <div className="FooterContainer">
+        <div className="FooterSelect">
+          <p className="FooterSelectQuick">Snabbval</p>
+          <p>Hitta bostad</p>
+          <p>Hyr ut din bostad</p>
+          <p>Om oss</p>
+        </div>
+        <div className="FooterContact">
+          <p>Kontakta oss</p>
+          <p>E-post: exempel@mail.com</p>
+          <p>Telefon: 0703-44 56 78</p>
+          <p>Adress: Exempelgatan 2</p>
+        </div>
+        <div className="FooterSocial">
+          <p>Instagram</p>
+          <p>Facebook</p>
+          <p>Twitter</p>
+        </div>
+      </div>
     </footer>
   );
 };

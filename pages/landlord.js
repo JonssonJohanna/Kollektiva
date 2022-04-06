41612a4ced939bb695f11c01e0acfeb5f3ec0cd7
JonssonJohannae <johanna_jonson@hotmail.com>
2022-04-06 13:54:09 +0200

    Added link to another page

--- pages/landlord.js
+++ pages/landlord.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 const landlord = () => {
   return (
     <div>
       <h1>Vill du hyra ut din bostad?</h1>
+
+      <Link href="StepOne">
+        <button>Step One</button>
+      </Link>
     </div>
   );
 };

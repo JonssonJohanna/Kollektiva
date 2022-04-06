b6e6ef1f4e57d98566ba581e643d5cef7456a16d
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-04-06 15:02:45 +0200

    added content to step one

--- pages/StepOne/index.js
+++ pages/StepOne/index.js
@@ -3,8 +3,64 @@ import styles from "/styles/StepOne.module.css";
 
 const StepOne = () => {
   return (
-    <div>
-      <h1 className={styles.test}>Step 1</h1>
+    <div className={styles.bodyContainer}>
+      <h1>Titel på sidan</h1>
+      <div className={styles.parentContainer}>
+        <div className={styles.leftContainer}>
+          <div className={styles.stepOne}>
+            <h3 class={styles.step}>Step 1</h3>
+            <p class={styles.stepInfo}>text</p>
+          </div>
+          <div className={styles.stepOne}>
+            <h3 class={styles.step}>Step 2</h3>
+            <p class={styles.stepInfo}>text</p>
+          </div>
+          <div className={styles.stepOne}>
+            <h3 class={styles.step}>Step 3</h3>
+            <p class={styles.stepInfo}>text</p>
+          </div>
+          <div className={styles.stepOne}>
+            <h3 class={styles.step}>Step 4</h3>
+            <p class={styles.stepInfo}>text</p>
+          </div>
+        </div>
+
+        <div className={styles.rightContainer}>
+          <div className={styles.info}>
+            <h2 className={styles.title}>Hej</h2>
+            <p className={styles.moreInfo}>More info </p>
+          </div>
+        </div>
+
+        <div className={styles.rightContainer}>
+          <div className={styles.info}>
+            <h2 className={styles.title}>Hej</h2>
+            <p className={styles.moreInfo}>More info </p>
+          </div>
+        </div>
+
+        <div className={styles.rightContainer}>
+          <div className={styles.info}>
+            <h2 className={styles.title}>Hej</h2>
+            <p className={styles.moreInfo}>More info </p>
+          </div>
+        </div>
+
+        <div className={styles.rightContainer}>
+          <div className={styles.info}>
+            <h2 className={styles.title}>Hej</h2>
+            <p className={styles.moreInfo}>More info </p>
+          </div>
+        </div>
+
+        <div className={styles.rightContainer}>
+          <div className={styles.info}>
+            <h2 className={styles.title}>Hej</h2>
+            <p className={styles.moreInfo}>More info </p>
+          </div>
+        </div>
+      </div>
+
       <Link href="StepTwo">
         <button>Next step</button>
       </Link>

1565af07bf4668dfbba147b01609b86d5907bba0
JonssonJohannae <johanna_jonson@hotmail.com>
2022-04-06 09:40:38 +0200

    Updated path

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import Footer from "../components/Footer";
-import Navbar from "../components/navbar";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 

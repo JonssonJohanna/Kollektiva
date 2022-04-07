61d0bcf0bd6a7fabf4015afb7d308243ce48a73b
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-04-07 14:13:33 +0200

    after live-share

--- pages/StepOne/index.js
+++ pages/StepOne/index.js
@@ -4,26 +4,26 @@ import styles from "/styles/StepOne.module.css";
 const StepOne = () => {
   return (
     <div className={styles.bodyContainer}>
-      <h1 className={styles.h1}>Steg 1</h1>
-      <h3 className={styles.breadText}>
+      <h2 className={styles.mainTitle}>Steg 1</h2>
+      <p className={styles.breadText}>
         Fyll i information om den bostaden du vill hyra ut
-      </h3>
+      </p>
       <div className={styles.parentContainer}>
         <div className={styles.leftContainer}>
-          <div className={styles.stepOne}>
-            <h3 class={styles.step}>Step 1</h3>
+          <div className={styles.active}>
+            <h4 class={styles.active}>Step 1</h4>
             <p class={styles.stepInfo}>text</p>
           </div>
           <div className={styles.stepOne}>
-            <h3 class={styles.step}>Step 2</h3>
+            <h4 class={styles.step}>Step 2</h4>
             <p class={styles.stepInfo}>text</p>
           </div>
           <div className={styles.stepOne}>
-            <h3 class={styles.step}>Step 3</h3>
+            <h4 class={styles.step}>Step 3</h4>
             <p class={styles.stepInfo}>text</p>
           </div>
           <div className={styles.stepOne}>
-            <h3 class={styles.step}>Step 4</h3>
+            <h4 class={styles.step}>Step 4</h4>
             <p class={styles.stepInfo}>text</p>
           </div>
         </div>
@@ -33,7 +33,7 @@ const StepOne = () => {
             <h2 className={styles.title}>Adress</h2>
             <form>
               <div className={styles.inputField}>
-                <label>Bostadens adress</label>
+                <label className={styles.adress}>Bostadens adress</label>
                 <input type="text" placeholder="Exempelgatan 1" />
               </div>
             </form>
@@ -52,12 +52,12 @@ const StepOne = () => {
             {/* <p className={styles.moreInfo}>More info </p> */}
           </div>
           <div className={styles.info}>
-            <h2 className={styles.title}>Hej</h2>
-            <p className={styles.moreInfo}>More info </p>
+            <h2 className={styles.title}>Typ av bostad</h2>
+            <h4 className={styles.moreInfo}>Vilken typ av bostad hyr du ut?</h4>
           </div>
           <div className={styles.info}>
             <h2 className={styles.title}>Hej</h2>
-            <p className={styles.moreInfo}>More info </p>
+            <p className={styles.moreInfo}> </p>
           </div>
           <div className={styles.info}>
             <h2 className={styles.title}>Hej</h2>
